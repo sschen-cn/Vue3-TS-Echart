c2b2d2ec326f815da24d32d733ddbca0f9e7eb4d
stu.chen <stuchen_cn@163.com>
2023-10-08 21:57:14 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">home</div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue';
+</script>
+
+<style lang="scss" scoped>
+$bgcolor: rgb(0, 81, 255);
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>home</div>
+</template>
+
+<script setup lang="ts">
+import { ref, reactive } from 'vue';
+console.log('home');
+</script>
+
+<style lang="scss" scoped></style>
